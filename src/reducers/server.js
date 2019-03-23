d0c165d2c52719d45959dcc78f8af6d9710de653
karim mohamed <karimmadnan@gmail.com>
2019-03-23 16:28:08 +0200

    serverFix

--- src/reducers/server.js
+++ src/reducers/server.js
@@ -1,6 +1,6 @@
 export default function() {
     return {
-        main: 'http://192.168.1.2:4000/server/',
+        main: 'http://ggegypt.com/server/',
         localHost: 'http://192.168.1.7:4000/server/'
     }
 }
