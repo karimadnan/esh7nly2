d997ef2ad7ba01d7c4afd4a098574bd47e441653
karim mohamed <karimmadnan@gmail.com>
2019-04-27 17:49:42 +0200

    SADFASDF

--- src/reducers/server.js
+++ src/reducers/server.js
@@ -1,6 +1,6 @@
 export default function() {
     return {
-        main: 'http://www.ggegypt.com/server',
+        main: 'https://www.ggegypt.com/server',
         localHost: 'http://192.168.1.4:4000/server/'
     }
 }
